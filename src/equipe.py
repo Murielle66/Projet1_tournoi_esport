decba5eee3d8ee02cc179419fc8e8fcf6f50dc0f
Murielle GDN <murielguedenon@gmail.com>
2026-04-20 19:28:34 +0200

    Mise en place des classes Joueur et Equipe et des structures de données

---
+++ src/equipe.py
@@ -0,0 +1,4 @@
+class Equipe:
+    def __init__(self, nom_equipe):
+        self.nom_equipe =nom_equipe
+        self.liste_joueurs = [] #ici on stockera des objets joueur
---
+++ src/joueur.py
@@ -0,0 +1,4 @@
+class Joueur :
+    def __init__(self, pseudo): 
+     self.pseudo= pseudo
+     self.score_total=0
