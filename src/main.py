c1ca167585713022dd4568386afc8fc82626968e
Murielle GDN <murielguedenon@gmail.com>
2026-04-21 21:00:51 +0200

    Architecture POO: Implémentation de l'encapsulation et de l'héritage(bloc3 suite)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from match import Match
+from entites import Organisatrice
 from joueur import Joueur
 from equipe import Equipe
 #1. Création des joueurs
@@ -10,3 +12,14 @@ ma_team.ajouter_joueur(j1)
 ma_team.ajouter_joueur(j2)
 #4.Vérification
 print(f"Nombre de joueurs dans l'équipe : {len(ma_team.liste_joueurs)}")
+#5. Test de l'héritage
+admin = Organisatrice("Murielle","Super-User")
+print(f"Admin :{admin.nom}, Role : {admin.role}")
+#6. Test de l'encapsulation
+j1.ajouter_points(10)
+print(f"Nouveau score de {j1.pseudo} : {j1.obtenir_score()} points")
+#7. Test de la modularité
+autre_team = Equipe("Aigles de cotonou")
+nouveau_match = Match(ma_team, autre_team)
+nouveau_match.designer_vainqueur("Les Apex")
+print(f"Vainqueur du match : {nouveau_mach.vainqueur.nom_equipe}")
